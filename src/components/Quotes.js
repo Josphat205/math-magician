2285302bfcaecbf160770efc902fd2d0c69110fc
lomantech <87063237+lomantech@users.noreply.github.com>
2022-07-05 17:12:44 +0300

    Add quote javascript file

---
+++ src/components/Quotes.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Quotes.css';
+
+function Quotes() {
+  return (
+    <div className="section">
+      <p>
+        Mathematics as an expression of the human mind reflects the active will,
+        the contemplative reason, and the desire for aesthetic perfection. Its
+        basic elements are logic and intuition, analysis and construction,
+        generality and individuality. — Richard Courant, German-American
+        mathematician, As far as the laws of mathematics refer to reality, they
+        are not certain, and as far as they are certain, they do not refer to
+        reality. — Albert Einstein
+      </p>
+    </div>
+  );
+}
+
+export default Quotes;
